404c7805ba1f8a11568ce4edfaa7fa52fdc78aa4
Mikias Tenalem <mikias.tenalem.ayenew@gmail.com>
2023-05-10 15:02:20 -0700

    minor bug fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(100):
-    print("{:2d}".format(i), end='')
+    print("{i:2d}".format(i), end='')
     if i < 99:
-        print (",", end='')
+        print(",", end='')
