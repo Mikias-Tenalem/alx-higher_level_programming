3b603017638b51c4299024b8eb04001adaaf2b2f
Mikias Tenalem <mikias.tenalem.ayenew@gmail.com>
2023-05-10 13:30:37 -0700

    tasks

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,6 +6,6 @@ if int(str(number)[-1]) > 5:
     last_str = 'and is greater than 5'
 elif int(str(number)[-1]) == 0:
     last_str = 'and is 0'
-elif int(str(number)[-1]) < 6 & int(str(number)[-1]) > 0:
+elif int(str(number)[-1]) < 6 & int(str(number)[-1]) != 0:
     last_str = 'and is less than 6 and not 0'
 print(f"Last digit of {number} is {str(number)[-1]} {last_str}")
