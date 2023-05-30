6990adbf9b264bb4b477772378735375c19a37e5
Mikias-Tenalem <mikias.tenalem.ayenew@gmail.com>
2023-05-30 12:05:48 +0300

    unique add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,5 +2,6 @@
 def uniq_add(my_list=[]):
     result = set(my_list)
     set_sum = 0
-    set_sum = set_sum + x for x in result
+    for i in result:
+        set_sum = set_sum + i
     return set_sum
