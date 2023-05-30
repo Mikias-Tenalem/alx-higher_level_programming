6dead117a923fc1ebfb50336a86e308e7f74ba37
Mikias-Tenalem <mikias.tenalem.ayenew@gmail.com>
2023-05-30 11:34:29 +0300

    final changes

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [list(map(lambda x: x ** 2, row)) for row in matrix]
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = print(square_matrix_simple(matrix))
+    return ([list(map(lambda x: x ** 2, row)) for row in matrix])
