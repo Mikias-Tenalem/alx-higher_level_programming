53ec2eedae1d408639a2aa8bb1a33fcf5d53bb5c
Mikias-Tenalem <mikias.tenalem.ayenew@gmail.com>
2023-05-30 19:36:33 +0300

    delete comment

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,7 +3,7 @@
 
 
 def weight_average(my_list=[]):
-    """Return the weighted average of all integers in a list of tuples."""
+    
     if not isinstance(my_list, list) or len(my_list) == 0:
         return (0)
 
