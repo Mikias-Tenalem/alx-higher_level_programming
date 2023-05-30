93f5744c581b37f69386acd2c4ea100c338e17a4
Mikias-Tenalem <mikias.tenalem.ayenew@gmail.com>
2023-05-30 07:15:28 +0300

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     while my_list.index(search) is True:
-        mylist[my_list.index(search)] = replace
+        my_list[my_list.index(search)] = replace
